074b1f92b49d1b6771a40361179f820d50b5f650
Shaktidhar <sdmohapatra7@gmail.com>
2023-06-03 09:59:33 +0530

    create  a timer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-
+import React from 'react';
 import TimerOne from './components/timer/TimerOne';
 
-function App() {
-  return (
-    <>
-    <TimerOne />
-    </>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      mount:false
+    }
+  }
+  handleMount=()=>{
+    this.setState((prevState)=>({mount:!prevState.mount}))
+  }
+  render(){
+    return (
+      <>
+      <button onClick={this.handleMount}>{this.state.mount?'Un-Mount':'Mount'}</button>
+      {this.state.mount?<TimerOne />:null}
+      </>
+    );
+  }
+  
 }
 
 export default App;
--- src/components/timer/TimerOne.js
+++ src/components/timer/TimerOne.js
@@ -4,8 +4,9 @@ export default class TimerOne extends Component {
     constructor(){
         super();
         this.state={
-            count:0
+            time:0
         }
+        this.timer =null;
         console.log('TimerOne Constructor');
     }
     static getDerivedStateFromProps(){
@@ -15,26 +16,31 @@ export default class TimerOne extends Component {
     shouldComponentUpdate(nextProps,nextState){
         return true;
     }
-    handleIncrease=()=>{
-        this.setState((prevState)=>{
-            return{
-                count:prevState.count +1
-            }
-        })
-    }
+    // handleIncrease=()=>{
+    //     this.setState((prevState)=>{
+    //         return{
+    //             count:prevState.count +1
+    //         }
+    //     })
+    // }
     render(){
         console.log('TimerOne Render');
         return(
             <>
-            <h1>Counter</h1>
+            {/* <h1>Counter</h1>
             <h2>{this.state.count}</h2>
-            <button onClick={this.handleIncrease}>Increase</button>
+            <button onClick={this.handleIncrease}>Increase</button> */}
+            <h1>Time Spent: {new Date (this.state.time *1000).toISOString().slice(11,19)}</h1>
+
             </>
         )
     }
     componentDidMount(){
         console.log('ComponentDidMount');
         console.log('__________________________________');
+        this.timer = setInterval(()=>{
+            this.setState((prevState)=>({time:prevState.time+1}))
+        },1000)
     }
     getSnapshotBeforeUpdate(pervProps,prevState){
         console.log('getSnapSortBeforeUpdate');
@@ -43,6 +49,12 @@ export default class TimerOne extends Component {
     componentDidUpdate(){
         console.log('TimerOne ComponentDidUpdate');
         console.log('__________________________________');
+        if(this.state.time===30){
+            clearInterval(this.timer);
+        }
+    }
+    componentWillUnmount(){
+        console.log('TimerOne ComponentWillUnmount');
     }
     
 }
